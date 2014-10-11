8e6d8ee533b627cd679a06353c5e5c6709bb1750
banseon <banseon@126.com>
2014-10-11 23:48:51 +0800

    第二次提交，增加了Log4j2的内容。

---
+++ src/main/java/com/ycbike/core/domain/Identifiable.java
@@ -0,0 +1,19 @@
+package com.ycbike.core.domain;
+
+import java.io.Serializable;
+
+public interface Identifiable extends Serializable {
+
+	/**
+	 * 获取主键
+	 * @return  返回组件值
+	 */
+	public String getId();
+	
+	/**
+	 * 设置ID属性
+	 * @param id
+	 */
+	public void setId(String id);
+	
+}
