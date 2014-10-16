c5dbd34771b0cfbdda0fcb12fb9b944d1c33ebfa
banseon <banseon@126.com>
2014-10-16 23:35:49 +0800

    第五次提交：分解了Spring的部分配置文件。spring-plugin-Resolver.xml spring-project.xml

---
+++ src/main/java/com/ycbike/web/dao/IYcBikeUserLoginDao.java
@@ -0,0 +1,8 @@
+package com.ycbike.web.dao;
+
+import com.ycbike.core.dao.BaseDao;
+import com.ycbike.core.model.YcBikeUserLogin;
+
+public interface IYcBikeUserLoginDao extends BaseDao<YcBikeUserLogin> {
+
+}
